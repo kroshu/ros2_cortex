61b25cfd396b1954fc366e16264c406bbb17d55c
Gergely Kovács <Gergely.Kovacs4@kuka.com>
2020-09-21 17:10:07 +0200

    Started implementing joint state publisher part of motion_tracker node

--- include/MotionTracker.hpp
+++ include/MotionTracker.hpp
@@ -3,6 +3,9 @@
 
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "std_msgs/msg/int8.hpp"
+#include "geometry_msgs/msg/point.hpp"
 
 class MotionTracker: public rclcpp_lifecycle::LifecycleNode {
 public:
@@ -27,10 +30,18 @@ public:
 	on_error(const rclcpp_lifecycle::State & state);
 private:
 	rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr marker_array_subscriber_;
+	rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::JointState>::SharedPtr reference_joint_state_publisher_;
+	rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr active_axis_changed_publisher_;
 	rclcpp::QoS qos;
 	rclcpp::message_memory_strategy::MessageMemoryStrategy<visualization_msgs::msg::MarkerArray>::SharedPtr msg_strategy;
 	std::function<void(visualization_msgs::msg::MarkerArray::ConstSharedPtr msg)> callback;
+	sensor_msgs::msg::JointState::SharedPtr reference_joint_state_;
+	std_msgs::msg::Int8::SharedPtr active_joint_msg_;
+	std::vector<double> lower_limits_rad_;
+	std::vector<double> upper_limits_rad_;
+	std::vector<double> segment_lengths_;
 	void markersReceivedCallback(visualization_msgs::msg::MarkerArray::ConstSharedPtr msg);
+	static double distBetweenPoses(geometry_msgs::msg::Point& first, geometry_msgs::msg::Point& second);
 };
 
 #endif
--- src/MotionTracker.cpp
+++ src/MotionTracker.cpp
@@ -1,25 +1,65 @@
 #include <memory>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+#include <iterator>
 
 #include "MotionTracker.hpp"
 #include "rclcpp/message_memory_strategy.hpp"
 #include "lifecycle_msgs/msg/state.hpp"
 #include "rclcpp/rclcpp.hpp"
 
-MotionTracker::MotionTracker(): rclcpp_lifecycle::LifecycleNode("motion_tracker"), qos(rclcpp::QoS(rclcpp::KeepLast(1))){
+MotionTracker::MotionTracker(): rclcpp_lifecycle::LifecycleNode("motion_tracker"), qos(rclcpp::QoS(rclcpp::KeepLast(1))),
+	lower_limits_rad_(7), upper_limits_rad_(7), segment_lengths_({0.1575,0.2025,0.2045,0.2155,0.1895,0.2155,0.081})
+{
 	qos.best_effort();
 	msg_strategy = std::make_shared<rclcpp::message_memory_strategy::MessageMemoryStrategy<visualization_msgs::msg::MarkerArray>>();
 	callback = [this](visualization_msgs::msg::MarkerArray::ConstSharedPtr msg) -> void
 				{markersReceivedCallback(msg);};
 }
 
-void MotionTracker::markersReceivedCallback(visualization_msgs::msg::MarkerArray::ConstSharedPtr msg){
-	for(auto it = msg->markers.begin(); it != msg->markers.end(); ++it){
-		RCLCPP_INFO(get_logger(), "Namespace: " + it->ns);
-		RCLCPP_INFO(get_logger(), "ID: " + std::to_string(it->id));
+double MotionTracker::distBetweenPoses(geometry_msgs::msg::Point& first, geometry_msgs::msg::Point& second){
+	return sqrt(pow(std::fabs(first.x-second.x),2)+pow(std::fabs(first.y-second.y),2)+pow(std::fabs(first.z-second.z),2));
+}
 
-		RCLCPP_INFO(get_logger(), "Point: x: " + std::to_string(it->pose.position.x) +
-				" y: " + std::to_string(it->pose.position.y) +
-				" z: " + std::to_string(it->pose.position.z));
+void MotionTracker::markersReceivedCallback(visualization_msgs::msg::MarkerArray::ConstSharedPtr msg){
+//	for(auto it = msg->markers.begin(); it != msg->markers.end(); ++it){
+//		RCLCPP_INFO(get_logger(), "Namespace: " + it->ns);
+//		RCLCPP_INFO(get_logger(), "ID: " + std::to_string(it->id));
+//
+//		RCLCPP_INFO(get_logger(), "Point: x: " + std::to_string(it->pose.position.x) +
+//				" y: " + std::to_string(it->pose.position.y) +
+//				" z: " + std::to_string(it->pose.position.z));
+//	}
+
+	std::vector<visualization_msgs::msg::Marker> joint_markers;
+	std::copy_if(msg->markers.begin(), msg->markers.end(), std::back_inserter(joint_markers),
+			[](visualization_msgs::msg::Marker marker)->bool{return marker.ns == "joint_markers";});
+
+	for(int active_joint=0; active_joint < 7; ++active_joint){
+		active_joint_msg_->data = active_joint+1;
+		active_axis_changed_publisher_->publish(*active_joint_msg_);
+//		double distance = distBetweenPoses(joint_markers[active_joint+1].pose.position, joint_markers[active_joint].pose.position);
+		double distance = 0.1575;
+		int ulp = 5;
+		if(std::fabs(segment_lengths_[active_joint]-distance) <= std::numeric_limits<double>::epsilon() * std::fabs(segment_lengths_[active_joint]+-distance)*ulp)
+			RCLCPP_ERROR(get_logger(), "Markers impossible to track with robot");
+		else{
+//			TODO
+//			Calculate rad position of joint based on the two endpositions (or it could be more than two)
+//			e.g. we calculate the relative position of the moving endpoint to the constant base endpoint
+//			- compare it to the relative pos at default state (probably pos = 0)
+			float calculated_pos = 0;
+			if(lower_limits_rad_[active_joint]*0.9 < calculated_pos && calculated_pos < upper_limits_rad_[active_joint]*0.9)
+			{
+				reference_joint_state_->position[active_joint] = calculated_pos;
+				reference_joint_state_publisher_->publish(*reference_joint_state_);
+			}
+			else
+			{
+				RCLCPP_WARN(get_logger(), "Joint limit reached!");
+			}
+		}
 	}
 }
 
@@ -62,6 +102,11 @@ rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 MotionTracker::on_activate(const rclcpp_lifecycle::State & state){
 	marker_array_subscriber_ = this->create_subscription<visualization_msgs::msg::MarkerArray>("markers",
 				qos, callback, rclcpp::SubscriptionOptions(), msg_strategy);
+	active_axis_changed_publisher_ = this->create_publisher<std_msgs::msg::Int8>("active_axis_changed", qos);
+	active_joint_msg_ = std::make_shared<std_msgs::msg::Int8>();
+	reference_joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("reference_joint_state", qos);
+	reference_joint_state_ = std::make_shared<sensor_msgs::msg::JointState>();
+	reference_joint_state_->position.resize(7);
 	return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
